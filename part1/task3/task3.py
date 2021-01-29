e803f15214ce96a3e9f220412906ccf4866a4f2e
Ruoxi Gao <rmapgao@ucl.ac.uk>
2021-01-29 14:12:31 +0000

    second submission

--- part1/task3/task3.py
+++ part1/task3/task3.py
@@ -5,18 +5,19 @@ EPOCH = 100
 learning_rate = 0.1
 tolerance = 1e-7
 
-
+# calculate gradient using finite difference
 def finite_difference_gradient(FX, x):  
     dfx = np.copy(x)
+    # calculate gradient on x(different directions) one by one
     for i in range(len(x)):
         x_plus = np.copy(x)
         x_minus = np.copy(x)
         x_plus[i] += 1e-5
         x_minus[i] -= 1e-5
         dfx[i] = (FX(x_plus) - FX(x_minus)) / (2*1e-5)
-    
     return dfx
 
+# graduent descent on FX, optimize x
 def gradient_descent(FX, x):    
     fx = FX(x)
     fx_list = [fx]
@@ -25,20 +26,23 @@ def gradient_descent(FX, x):
         dfx = finite_difference_gradient(FX, x) 
         x += -learning_rate * dfx  
         fx = FX(x)
-
         if fx - fx_last > -tolerance:
             break
         fx_list.append(fx)     
     return fx_list, x
 
-#(A+Bx1+Cx2+Dx3)^2
+# calculate polynomial according to a
 def quadratic_polynomial(a, x):   
     return a[0] + a[1]*x[0]**2 + a[2]*x[1]**2 + a[3]*x[2]**2 + a[4]*x[0]*x[1] + \
            a[5]*x[0]*x[2] + a[6]*x[1]*x[2] + a[7]*x[0] + a[8]*x[1] + a[9]*x[2]
-           
+ 
+# add some control on a to convert objective function into format of (A+Bx1+Cx2+Dx3)^2
+# which is definitely convex function          
 def a_cal(convex_paras): 
     a = np.zeros(10)
-    a[0:4] = convex_paras[0:4]*convex_paras[0:4]
+    # a0=A^2, a1=A^2, a2=A^2, a3=A^2
+    # a4=2BC, a5=2BD, a6=2CD, a7=2AB a8=2AC, a9=2AD
+    a[0:4] = convex_paras[0:4]**2
     a[4] = 2*convex_paras[1]*convex_paras[2]
     a[5] = 2*convex_paras[1]*convex_paras[3]
     a[6] = 2*convex_paras[2]*convex_paras[3]
@@ -50,12 +54,16 @@ def a_cal(convex_paras):
 if __name__ == '__main__':
     # convex_paras(A B C D) in range(-2,2)
     convex_paras = (np.random.rand(4)-0.5)*4
+    # calculate a from convex_paras
     a = a_cal(convex_paras)
-    # A+Bx1+Cx2+Dx3 = 0
-    # initial estimate: x = 0s, where f(x) = D^2 in range(0,4)
+    # minimum when A+Bx1+Cx2+Dx3 = 0
+    # initial estimate: x0 = 0s, where f(x0) = A^2 in range(0,4)
     x = np.zeros(3)
     FX = lambda x: quadratic_polynomial(a, x)
+    # solve minimum by using gradient descent
     fx_list, solution = gradient_descent(FX, x)
+    
+    # plot gradient descent process and show results
     plt.plot(np.arange(len(fx_list)), fx_list)
     plt.xlabel("epoch", fontsize=15)
     plt.ylabel("fx", fontsize=15)
--- part1/task4/task4.py
+++ part1/task4/task4.py
@@ -2,26 +2,39 @@ import numpy as np
 from matplotlib import pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 
+# find neighbors from triangles and vertice number
 def find_neighbors(triangles, vertice):
     positions = np.where(triangles==vertice)
+    # find triangles which contain the vertice number
     neighbors = triangles[positions[0],:]
+    # calculate positions of the vertice in all neighbor triangles
     index = np.arange(len(positions[1]))*3 + positions[1]
+    # delete the vertice and repeated neighbors to generate all neighbors' numbers of the vertice
     neighbors = np.delete(neighbors, index)
     neighbors = np.unique(neighbors)
     return neighbors.astype(int)
-  
+
+#  lowpass mesh smoothing
 def lowpass_mesh_smoothing(triangles, vertices, iterations=10, Lambda=0.9, Mu=-1.02*0.9):
     first_vertices = np.copy(vertices)
     second_vertices = np.copy(vertices)
+    # iterate mesh smoothing algorithm
     for iteration in range(iterations):
+        # mesh smoothing for each verticle
         for i in range(np.size(vertices,0)):
+            # find all neighbors' numbers of the ith verticle 
             neighbors = find_neighbors(triangles, i)
+            # calculate correspinding neighbors' coordinates of the ith verticle
             neighbors_coordinate = vertices[neighbors,:]
+            # do the first mesh smoothing according to paper
             first_vertices[i,:] = vertices[i,:] + Lambda/len(neighbors)* \
                                np.sum(neighbors_coordinate - vertices[i,:], axis=0)
+            # update neighbors' coordinates of the ith verticle after the first mesh smoothing
             neighbors_coordinate = first_vertices[neighbors,:]
+            # do the second mesh smoothing according to paper
             second_vertices[i,:] = first_vertices[i,:] + Mu/len(neighbors)* \
                                np.sum(neighbors_coordinate - first_vertices[i,:], axis=0)
+        # update filtered vertices            
         vertices = second_vertices
     return vertices
         
@@ -30,10 +43,12 @@ if __name__ == '__main__':
     triangles = np.genfromtxt("example_triangles.csv",delimiter=',')-1
     vertices = np.genfromtxt("example_vertices.csv",delimiter=',')
     
+    # smooth the surface mesh with three different numbers of iterations, 5, 10, 25
     vertices_5 = lowpass_mesh_smoothing(triangles, vertices, iterations=5)
     vertices_10 = lowpass_mesh_smoothing(triangles, vertices_5, iterations=10-5)
     vertices_25 = lowpass_mesh_smoothing(triangles, vertices_10, iterations=25-10)
     
+    # plot results
     fig = plt.figure()
     ax = Axes3D(fig)
     ax.plot_trisurf(vertices[:,0], vertices[:,1], vertices[:,2], triangles=triangles, color='b', alpha=0.3)
